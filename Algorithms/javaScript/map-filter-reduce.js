b0da677c6f0063886cda2ec554c3f0846bfab612
rameez <rameez.manoli786@gmail.com>
2023-09-17 21:32:14 +0530

    map

--- Algorithms/javaScript/map-filter-reduce.js
+++ Algorithms/javaScript/map-filter-reduce.js
@@ -1,8 +1,14 @@
 const array = [5,1,3,2,6];
 
-//Transform it//
+//Transformation logic//
 
 //Double = [10,2,6,4,12]
 
 //Triple = [15,3,9,6,18]
 
+function double(x){
+    return x * 2;
+}
+
+const output = array.map(double)
+console.log(output)
