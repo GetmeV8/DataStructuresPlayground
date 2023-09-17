828121d2ce4348793b801390a4480e8a9fddc96f
rameez <rameez.manoli786@gmail.com>
2023-09-17 21:40:11 +0530

    filter

--- Algorithms/javaScript/map-filter-reduce.js
+++ Algorithms/javaScript/map-filter-reduce.js
@@ -1,14 +1,26 @@
-const array = [5,1,3,2,6];
+const array = [5, 1, 3, 2, 6];
 
+
+//**Map**//
 //Transformation logic//
 
 //Double = [10,2,6,4,12]
-
 //Triple = [15,3,9,6,18]
 
-function double(x){
+function double(x) {
     return x * 2;
 }
-
+const binaryOutput = array.map((x) => x.toString(2));
 const output = array.map(double)
 console.log(output)
+
+//**Filter**//
+
+function isOdd(x) {
+    return x % 2;
+    //return x%2===0;
+    //return x>4;
+}
+const filterOutput= array.filter(isOdd);
+console.log(filterOutput);
+
