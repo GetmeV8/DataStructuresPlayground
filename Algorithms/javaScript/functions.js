636bd58cb469bf77d6e2692c7ff9e64bb9bcb6b8
rameez <rameez.manoli786@gmail.com>
2023-09-18 17:16:14 +0530

    reduce

---
+++ Algorithms/javaScript/functions.js
@@ -0,0 +1,14 @@
+// Function Statements
+
+// Function Declaration
+
+// Anonymous Function
+
+// Named Function Expression
+
+// Difference between Parameters & Arguments ?
+
+// First Class Functions
+
+// Arrow Functions
+
