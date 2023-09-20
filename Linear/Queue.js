4f921239196e4c42ffe815d4f583995cd2fc0541
rameez <rameez.manoli786@gmail.com>
2023-09-20 12:16:56 +0530

    Queue init

---
+++ Linear/Queue.js
@@ -0,0 +1,28 @@
+                                // Queue //
+
+
+// The queue data structure is a sequential collection of elements that follows
+
+// principle of First In First Out (FIFO)
+
+// The first element inserted into the queue is first element to be removed
+
+// A queue of people. People enter the queue at one end (rear/tail) and leave
+
+// queue from the other end (front/ head).
+
+// Queue is an abstract data type. It is defined by its behavior rather than being
+
+// mathematical model
+
+// The Queue data structure supports two main operations
+
+// Enqueue, which adds an element to the rear/tail of the collection
+
+
+class Queue{
+    constructor(){
+        this.items=[]
+    }
+}
+
