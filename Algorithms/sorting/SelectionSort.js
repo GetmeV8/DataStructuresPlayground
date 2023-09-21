c3874ef7762d0fa507256f90137d04f1b081e1f7
rameez <rameez.manoli786@gmail.com>
2023-09-21 11:51:55 +0530

    selection sort

---
+++ Algorithms/sorting/SelectionSort.js
@@ -0,0 +1,22 @@
+function selectionSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            // Swap elements
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+const arr = [64, 25, 12, 22, 11];
+const sortedArr = selectionSort(arr);
+
+console.log(sortedArr); // Output: [11, 12, 22, 25, 64]
