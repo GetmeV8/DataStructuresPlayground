11a2f31a29a797fbfaeb9a9f262851e97ab0da09
rameez <rameez.manoli786@gmail.com>
2023-09-21 12:01:39 +0530

    selectionsort

--- Algorithms/sorting/SelectionSort.js
+++ Algorithms/sorting/SelectionSort.js
@@ -1,9 +1,8 @@
 function selectionSort(arr) {
-    const len = arr.length;
-    for (let i = 0; i < len - 1; i++) {
+    for (let i = 0; i < arr.length - 1; i++) {
         let minIndex = i;
 
-        for (let j = i + 1; j < len; j++) {
+        for (let j = i + 1; j < arr.length; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
