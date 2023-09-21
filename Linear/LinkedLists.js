f7504b1e1b8ad9ec857716cf67f53ac767b03594
rameez <rameez.manoli786@gmail.com>
2023-09-21 10:16:45 +0530

    Linked List

---
+++ Linear/LinkedLists.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    append(data) {
+        const newNode = new Node(data);
+
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+
+        let current = this.head;
+        while (current.next !== null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    prepend(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    display() {
+        let current = this.head;
+        while (current !== null) {
+            console.log(current.data);
+            current = current.next;
+        }
+    }
+}
+
+// Create a new linked list
+const myList = new LinkedList();
+
+// Append nodes
+myList.append(1);
+myList.append(2);
+myList.append(3);
+
+// Prepend a node
+myList.prepend(0);
+
+// Display the linked list
+myList.display();
