788d07e4a0c80d451286808a32f7c36c7d0dd3d5
rameez <rameez.manoli786@gmail.com>
2023-09-27 10:44:02 +0530

    object destructuring

--- Algorithms/JavaScript/destructuring.js
+++ Algorithms/JavaScript/destructuring.js
@@ -9,3 +9,18 @@ console.log(second); // Output: 2
 console.log(fourth); // Output: 4
 
 
+// Example object
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    city: 'New York'
+  };
+  
+  // Destructuring
+  const { firstName, lastName, age } = person;
+  
+  console.log(firstName); // Output: John
+  console.log(lastName);  // Output: Doe
+  console.log(age);        // Output: 30
+  
