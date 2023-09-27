6e9c1d30911f6c5093ba590fc106e082376fac21
rameez <rameez.manoli786@gmail.com>
2023-09-27 10:43:28 +0530

    destructuring

---
+++ Algorithms/JavaScript/destructuring.js
@@ -0,0 +1,11 @@
+// Example array
+const numbers = [1, 2, 3, 4, 5];
+
+// Destructuring
+const [first, second, , fourth] = numbers;
+
+console.log(first);  // Output: 1
+console.log(second); // Output: 2
+console.log(fourth); // Output: 4
+
+
